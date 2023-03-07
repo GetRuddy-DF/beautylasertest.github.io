466de24cf53453991b43acee3d2ae37081be24c3
GetRuddy-DF <37291767+GetRuddy-DF@users.noreply.github.com>
2023-03-08 04:26:22 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,39 @@ function slowScroll(id) {
     }, 500);
 }
 
+$("#sendMail").on("click", function() {
+    var name = $("#name").val().trim();
+    var phone = $("#phone").val().trim();
+
+    if(name == "") {
+        $("#errorMess").text("Введите имя");
+        return false;
+    } else if (phone == "") {
+        $("#errorMess").text("Введите номер");
+        return false;
+    }
+
+    $("#errorMess").text("");
+
+    $.ajax({
+        url: 'ajax/mail.php',
+        type: 'POST',
+        cache: false,
+        data: {'name': name, 'phone': phone},
+        dataType: 'html',
+        beforSend: function() {
+            $("#sendMail").prop("disabled", true);
+        },
+        succes: function(data) {
+            if(!data)
+            alert("Произошла ошибка сообщение не отправлено!");
+            else 
+            $("#form_input").trigger("reset");
+
+            $("#sendMail").prop("disabled", false);
+        }
+    });
+});
 
 $(document).on("scroll", function () {
     if($(window).scrollTop() === 0)
