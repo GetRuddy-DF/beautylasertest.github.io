4ae48d1f073b3669305fa361645feec4a19839ca
GetRuddy-DF <37291767+GetRuddy-DF@users.noreply.github.com>
2023-03-10 20:54:43 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -75,3 +75,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active-slide";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+// 
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
