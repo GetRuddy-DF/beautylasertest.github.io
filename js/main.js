aa54037cb0174073b4d2872f7760054e6be9522f
GetRuddy-DF <37291767+GetRuddy-DF@users.noreply.github.com>
2023-03-12 04:03:18 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function slowScroll(id) {
         scrollTop: $(id).offset().top,
         scrollPeeling: $(id).offset().peeling,
         scrollPrice: $(id).offset().price,
-        scrollAdvantages: $(id).offset().advantages,
+        scrollFeedback: $(id).offset().feedback,
         scrollMaster: $(id).offset().master,
         scrollContacts: $(id).offset().contacts
     }, 500);
@@ -80,7 +80,36 @@ function showSlides() {
 function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  
+  function dropDown2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  function dropDown3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+  }
+  function dropDown4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+  }
+  function dropDown5() {
+    document.getElementById("myDropdown5").classList.toggle("show");
+  }
+  function dropDown6() {
+    document.getElementById("myDropdown6").classList.toggle("show");
+  }
+  function dropDown7() {
+    document.getElementById("myDropdown7").classList.toggle("show");
+  }
+  function dropDown8() {
+    document.getElementById("myDropdown8").classList.toggle("show");
+  }
+  function dropDown9() {
+    document.getElementById("myDropdown9").classList.toggle("show");
+  }
+  function dropDown10() {
+    document.getElementById("myDropdown10").classList.toggle("show");
+  }
+  function dropDown11() {
+    document.getElementById("myDropdown11").classList.toggle("show");
+  }
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
